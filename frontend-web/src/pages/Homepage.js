251ae9ab335995d5be0f9dfed2fbf3972fb564d7
hoanggum <nguyenhuyhoang20032020@gmail.com>
2024-10-06 13:00:04 +0700

    feat: implemented/updated layout feed

---
+++ frontend-web/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../layouts/Layout';
+import Feed from '../components/Feed/Feed';
+
+function Homepage() {
+    return (
+      <Layout>
+       <Feed />
+      </Layout>
+    );
+}
+export default Homepage;
