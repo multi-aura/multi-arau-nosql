500f420e5e2d38d036ce1763cf3ad9d1ea049cab
HTrongThang <trongthang412003@gmail.com>
2024-10-15 13:10:52 +0700

    feat: updated something sidebar

--- frontend-web/src/components/Messages/MessageItem/MessageItem.js
+++ frontend-web/src/components/Messages/MessageItem/MessageItem.js
@@ -5,9 +5,15 @@ const MessageItem = ({ message, onClick }) => {
   const user = message.users && message.users.length > 0 ? message.users[0] : null;
   const avatar = user && user.avatar ? user.avatar : 'https://www.w3schools.com/w3images/avatar2.png';
 
+  // Kiểm tra nội dung tin nhắn cuối cùng
   const lastMessage = message.chats && message.chats.length > 0
-    ? message.chats[message.chats.length - 1].content.text ||
-    (message.chats[message.chats.length - 1].content.image ? 'Sent an image' : 'Sent a voice message')
+    ? (message.chats[message.chats.length - 1].content.text 
+      ? message.chats[message.chats.length - 1].content.text 
+      : message.chats[message.chats.length - 1].content.image 
+      ? 'Hình ảnh' // Hiển thị chữ 'Hình ảnh' nếu là hình ảnh
+      : message.chats[message.chats.length - 1].content.voice_url
+      ? 'Ghi âm'   // Hiển thị chữ 'Ghi âm' nếu là voice message
+      : 'No recent messages')
     : 'No recent messages';
 
   // Lấy thời gian của tin nhắn cuối cùng
@@ -32,29 +38,23 @@ const MessageItem = ({ message, onClick }) => {
 
   return (
     <li className="list-group-item d-flex align-items-center message-item" style={{ borderBottom: "1px solid white" }} onClick={onClick}>
-      {/* Avatar của người dùng */}
       <img src={avatar} alt="profile" className="avatar rounded-circle me-3" />
 
-      {/* Thông tin cuộc trò chuyện */}
       <div className="message-info">
         <strong className="message-name">{message.name_conversation || (user && user.fullname)}</strong>
 
-        {/* Tin nhắn và thời gian trên cùng một dòng */}
         <div className="last-message-container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <p className="small message-last" style={{ color: "#e9edef", marginRight: '10px' }}>
-            {lastMessage}
+            {lastMessage} {/* Hiển thị tin nhắn cuối cùng */}
           </p>
 
-          {/* Hiển thị thời gian tin nhắn cuối cùng */}
           {timeDisplay && (
-            <span className="small" style={{color:"#6f787d"}}>
+            <span className="small" style={{ color: "#6f787d" }}>
               {timeDisplay}
             </span>
           )}
         </div>
       </div>
-
-
     </li>
   );
 };
--- frontend-web/src/layouts/Layout.js
+++ frontend-web/src/layouts/Layout.js
@@ -2,19 +2,15 @@ import React from 'react';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Header from '../components/Header/Header';
 import './Layout.css';
+
 function Layout({ children, userData }) {
   return (
-    <div className="container-fluid p-0 w-full">
-      <div className="row no-gutters">
-        <div className="col-2 bg-dark text-white p-0 w-full">
-          <Sidebar />
-        </div>
-
-        <div className="col-10 bg-dark d-flex flex-column p-0">
-          <Header userData={userData} />
-          <div className="content-area bg-black flex-grow-1 p-1">
-            {children}
-          </div>
+    <div className="layout">
+      <Sidebar />
+      <div className="main-content">
+        <Header userData={userData} />
+        <div className="content-area">
+          {children}
         </div>
       </div>
     </div>
