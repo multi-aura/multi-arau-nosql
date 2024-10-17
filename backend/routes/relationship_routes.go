32b5221f3d439017e0e4c481477e1f82210bc1e3
mthuwhihand <vnn159357@gmail.com>
2024-10-17 23:38:13 +0700

    feat: implemented api get blocked users

--- backend/routes/relationship_routes.go
+++ backend/routes/relationship_routes.go
@@ -16,7 +16,7 @@ func SetupRelationshipRoutes(app *fiber.App) {
 
 	relationships := app.Group("/relationships")
 
-	relationships.Post("/status/:userID", middlewares.AuthMiddleware(), controller.GetRelationshipStatus)	
+	relationships.Post("/status/:userID", middlewares.AuthMiddleware(), controller.GetRelationshipStatus)
 	relationships.Post("/follow/:userID", middlewares.AuthMiddleware(), controller.Follow)
 	relationships.Delete("/unfollow/:userID", middlewares.AuthMiddleware(), controller.UnFollow)
 	relationships.Post("/block/:userID", middlewares.AuthMiddleware(), controller.Block)
@@ -24,5 +24,6 @@ func SetupRelationshipRoutes(app *fiber.App) {
 	relationships.Get("/friends", middlewares.AuthMiddleware(), controller.GetFriends)
 	relationships.Get("/followers", middlewares.AuthMiddleware(), controller.GetFollowers)
 	relationships.Get("/followings", middlewares.AuthMiddleware(), controller.GetFollowings)
+	relationships.Get("/blocked", middlewares.AuthMiddleware(), controller.GetBlockedUsers)
 	app.Get("/:username", middlewares.AuthMiddleware(), controller.GetProfile)
 }
